78bf02d9df5f6ed956206a76e85482a2d416ec39
Delph1 <andreas.galistel@gmail.com>
2024-08-08 11:26:41 +0200

    Less tests for now

--- tests.py
+++ tests.py
@@ -4,29 +4,37 @@ import subprocess
 from unittest import mock
 from fastapi.testclient import TestClient
 
-from main import app
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+from main import app, get_db
 from fetch_municipalities import fetch_municipalities
 
 #Setting up test client
 
 client = TestClient(app)
 
+# SQLAlchemy setup for testing
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+# Dependency override
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+app.dependency_overrides[get_db] = override_get_db
+
 # Main.py tests below this line
 
 ''' Test read main '''
 def test_read_main():
     response = client.get("/")
-    assert response.status_code == 200
-
-''' Test read municipalities '''
-def test_read_municipalities():
-    
-    response = client.get("/municipalities/")
-    assert response.status_code == 200
-
-''' Test read structured municipality data '''
-def test_read_structured_municipality_data():
-    response = client.get("/structured_municipality_data/1984?kpi_id=N00003")
     assert response.status_code == 200
 
 # Tests for fetch_municipalities.py
