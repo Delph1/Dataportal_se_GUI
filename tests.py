9e8b18ebac065706ddb7b973fd2314fc4792c189
Delph1 <andreas.galistel@gmail.com>
2024-08-08 11:31:34 +0200

    DB changes to tests

--- tests.py
+++ tests.py
@@ -7,18 +7,20 @@ from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-from main import app, get_db
 from fetch_municipalities import fetch_municipalities
 
 #Setting up test client
 
-client = TestClient(app)
+
 
 # SQLAlchemy setup for testing
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+# Create all tables in the test database
+Base.metadata.create_all(bind=engine)
 
 # Dependency override
 def override_get_db():
@@ -27,6 +29,10 @@ def override_get_db():
         yield db
     finally:
         db.close()
+
+from main import app, get_db
+
+client = TestClient(app)
 
 app.dependency_overrides[get_db] = override_get_db
 
